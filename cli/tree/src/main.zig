397f3ce79a4a6c4a61d9f638c71445fcc82f8842
Direwolfesp <alvarolg365@gmail.com>
2026-06-08 15:58:47 +0200

    add some arguments

--- cli/tree/src/main.zig
+++ cli/tree/src/main.zig
@@ -1,27 +1,32 @@
 const std = @import("std");
-const builtin = @import("builtin");
 const Io = std.Io;
 const Allocator = std.mem.Allocator;
+const builtin = @import("builtin");
+
+const clap = @import("clap");
+
+const log = std.log.scoped(.tree);
 
 pub const TreeWalker = struct {
     out: *Io.Writer,
     num_directories: u32 = 0,
     num_files: u32 = 0,
+    show_all: bool = false,
 
     const inner_symbols = [_][]const u8{ "├── ", "│   " };
     const outer_symbols = [_][]const u8{ "└── ", "    " };
 
-    const Self = @This();
+    const WalkError = Io.Writer.Error || Io.Dir.OpenError || Allocator.Error;
 
-    pub fn init(out: *Io.Writer) Self {
-        return .{ .out = out };
+    pub fn init(out: *Io.Writer, show_all: bool) TreeWalker {
+        return .{ .out = out, .show_all = show_all };
     }
 
-    pub fn summary(self: *Self) !void {
+    pub fn summary(self: *TreeWalker) !void {
         try self.out.print("{d} directories, {d} files\n", .{ self.num_directories, self.num_files });
     }
 
-    fn walkInner(self: *Self, io: Io, alloc: Allocator, dir: Io.Dir, prefix: []const u8) !void {
+    fn walkInner(self: *TreeWalker, io: Io, alloc: Allocator, dir: Io.Dir, prefix: []const u8) WalkError!void {
         var paths: std.ArrayList(Io.Dir.Entry) = .empty;
         defer {
             for (paths.items) |entry| alloc.free(entry.name);
@@ -44,6 +49,9 @@ pub const TreeWalker = struct {
         }.sort);
 
         for (paths.items, 0..) |entry, i| {
+            if (!self.show_all and entry.name[0] == '.')
+                continue;
+
             const symbols = if (i != paths.items.len - 1)
                 TreeWalker.inner_symbols
             else
@@ -64,29 +72,60 @@ pub const TreeWalker = struct {
         }
     }
 
-    pub fn walk(self: *Self, io: Io, alloc: Allocator, directory: []const u8) !void {
-        try self.out.print("{s}\n", .{directory});
+    pub fn walk(self: *TreeWalker, io: Io, alloc: Allocator, directory: []const u8) WalkError!void {
         const root = try Io.Dir.cwd().openDir(io, directory, .{ .iterate = true });
         defer root.close(io);
+        self.num_directories += 1;
+        try self.out.print("{s}\n", .{directory});
         try self.walkInner(io, alloc, root, "");
         try self.out.writeByte('\n');
     }
 };
 
 pub fn main(init: std.process.Init) !void {
-    const gpa = if (builtin.mode == .Debug) init.gpa else std.heap.smp_allocator;
-    const io = init.io;
-    const args = try init.minimal.args.toSlice(gpa);
-    defer gpa.free(args);
+    const params = comptime clap.parseParamsComptime(
+        \\-h, --help         Display this help and exit.
+        \\-a, --all          Show hidden files and directories.
+        \\<str>...           Directories to show. (Defaults to '.')
+        \\
+    );
 
-    const dir_name = if (args.len > 1) args[1] else ".";
+    var diag = clap.Diagnostic{};
+    var res = clap.parse(clap.Help, &params, clap.parsers.default, init.minimal.args, .{
+        .diagnostic = &diag,
+        .allocator = init.gpa,
+    }) catch |err| {
+        try diag.reportToFile(init.io, .stderr(), err);
+        return err;
+    };
+    defer res.deinit();
+
+    if (res.args.help != 0)
+        return clap.helpToFile(init.io, .stderr(), clap.Help, &params, .{});
 
     var stdout_buf: [4096]u8 = undefined;
-    var stdout_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buf);
+    var stdout_writer: Io.File.Writer = .init(.stdout(), init.io, &stdout_buf);
     const stdout = &stdout_writer.interface;
 
-    var w: TreeWalker = .init(stdout);
-    try w.walk(io, gpa, dir_name);
+    const show_all = res.args.all != 0;
+    var directories = blk: {
+        var arr: std.ArrayList([]const u8) = .empty;
+        for (res.positionals[0]) |pos|
+            try arr.append(init.gpa, pos);
+        if (arr.items.len == 0)
+            try arr.append(init.gpa, ".");
+        break :blk arr;
+    };
+    defer directories.deinit(init.gpa);
+
+    var w: TreeWalker = .init(stdout, show_all);
+    for (directories.items) |dir| {
+        w.walk(init.io, init.gpa, dir) catch |err| switch (err) {
+            error.FileNotFound => log.err("Directory '{s}' not found", .{dir}),
+            error.NotDir => log.err("'{s}' is not a directory", .{dir}),
+            else => |e| return e,
+        };
+    }
     try w.summary();
     try stdout.flush();
 }
