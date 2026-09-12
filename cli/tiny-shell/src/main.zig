f8f994e1d11d4f00d78f5e2e62b53f09a726c5e0
Direwolfesp <alvarolg365@gmail.com>
2026-09-12 13:27:55 +0200

    shell: implement cd builtin

--- cli/tiny-shell/src/main.zig
+++ cli/tiny-shell/src/main.zig
@@ -8,12 +8,11 @@ const prompt = "$ ";
 const exit_msg = "bye user!";
 const default_PATH = "/usr/local/bin:/bin/:/usr/bin";
 
-fn intHandler(sig: linux.SIG) callconv(.c) void {
-    _ = sig;
-}
+fn intHandler(_: linux.SIG) callconv(.c) void {}
 
 pub fn main(init: std.process.Init) !void {
     const arena = init.arena.allocator();
+    const io = init.io;
 
     var sa: linux.Sigaction = .{
         .flags = 0,
@@ -46,15 +45,28 @@ pub fn main(init: std.process.Init) !void {
         const line = std.mem.trim(u8, line_raw, &std.ascii.whitespace);
         if (line.len == 0) continue;
 
-        if (std.mem.eql(u8, line, "exit")) {
-            std.debug.print("{s}\n", .{exit_msg});
-            break;
-        }
-
         var args: std.ArrayList([]const u8) = .empty;
         var it = std.mem.tokenizeAny(u8, line, &std.ascii.whitespace);
         while (it.next()) |word| {
-            try args.append(arena, word);
+            const path = try std.fs.path.resolve(arena, &.{word});
+            try args.append(arena, path);
+        }
+
+        // basic builtin commands
+        if (std.mem.eql(u8, args.items[0], "exit")) {
+            std.debug.print("{s}", .{exit_msg});
+            break;
+        } else if (std.mem.eql(u8, args.items[0], "cd")) {
+            const dest = if (args.items.len > 1)
+                args.items[1]
+            else if (init.environ_map.get("HOME")) |home_dir|
+                home_dir
+            else
+                try std.process.currentPathAlloc(io, arena);
+            std.process.setCurrentPath(io, dest) catch |err| {
+                std.log.err("cd: '{s}' does not exist ({t})", .{ dest, err });
+            };
+            continue;
         }
 
         const pid = linux.fork();
